afa3c51db7f8e6eba6383274b56f17133a19a6d5
Sombabu Patel <133793786+Sombabu25@users.noreply.github.com>
2025-02-20 06:05:56 +0000

    data types

---
+++ basic1/variable.js
@@ -0,0 +1,27 @@
+// string
+let name = "sombabu"
+let branch = 'cse'
+
+//number
+let age=20
+let mobile=8210787484
+
+//object
+let details = {name:"som", age:20, branch:"cse"}
+
+// array object store multiple data types
+const  score = [10,20,30,40]
+
+console.log(typeof name)
+console.log(typeof branch)
+
+console.log(typeof age)
+console.log(typeof mobile)
+
+console.log(typeof details)
+
+console.log(typeof score)
+console.log(score.length)
+console.log(score[0])
+score.push(80)
+console.log(score)
